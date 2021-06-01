8f3edadda9e1989bf35ff1b1a2fefe581711b2a3
pguyon <pguyon69@gmail.com>
2021-06-01 10:29:48 +0200

    up

--- connect.php
+++ connect.php
@@ -1,10 +1,32 @@
+<?php
+include 'parts/global-stylesheet.php';
+if (!isset($_SESSION) || !isset($_SESSION['username'])) {
+    header('location: login.php');
+}
+?>
 <html>
 
-<head></head>
+<head>
+</head>
 
 <body>
-    <h1>Connect</h1>
-    <a href="login.php">Retour login</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Acceuil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="disconnect.php">Se déconnecter</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </body>
 
 </html>
--- login.php
+++ login.php
@@ -18,7 +18,7 @@ session_start();
         </div>
         <form action="manager/login-manager.php" method="post">
             <input type="text" id="username" class="fadeIn second" name="username" placeholder="username">
-            <input type="text" id="password" class="fadeIn third" name="password" placeholder="password">
+            <input type="password" id="password" class="fadeIn third" name="password" placeholder="password">
             <input type="submit" class="fadeIn fourth" value="Log In">
         </form>
         <div class="form-group error">
